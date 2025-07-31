402986e06e2336c69616575313d695ffa42bb5ce
daham-13 <dahamchinthana2003@gmail.com>
2025-07-31 23:10:13 +0530

    Day one

---
+++ ExcelReader.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+class ExcelReader:
+    
+    def __init__ (self, file_href)
+        self.data_frame = pd.read_excel(file_href)
+        self.raw_data = {}
+    
+    def read_data():
+        
+
