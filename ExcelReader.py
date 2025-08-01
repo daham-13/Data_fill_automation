15570a656e40efef248c54de610c85cf4d85f1f2
daham-13 <dahamchinthana2003@gmail.com>
2025-08-01 23:18:25 +0530

    Read data amd update the dictionary

--- ExcelReader.py
+++ ExcelReader.py
@@ -2,10 +2,35 @@ import pandas as pd
 
 class ExcelReader:
     
-    def __init__ (self, file_href)
-        self.data_frame = pd.read_excel(file_href)
-        self.raw_data = {}
+    def __init__ (self, file_href):
+        self.data_frame = pd.read_excel(file_href, header=None)
+        self.raw_data = {
+            "surname":"",
+            "other_name":"",
+            "dob":"",
+            "P_num":"",
+            "P_exp":"",
+            "sex":"",
+            "nationality":"",
+            "email":"",
+            "nic":"",
+            "phone1":"",
+            "phone2":"",
+            "pob":"",
+            "cob":"",
+            "marital":"",
+            "address":"",
+            "d_name":"",
+            "d_passport":"",
+            "dnic":"",
+        }
     
-    def read_data():
-        
-
+    def read_data(self):
+        key_list = list(self.raw_data.keys())
+        # Loop over each key and assign the first cell in the corresponding row to the raw_data dictionary
+        for i, key in enumerate(key_list):
+            if i < len(self.data_frame):
+                self.raw_data[key] = self.data_frame.iloc[i, 0]
+            else:
+                self.raw_data[key] = ""
+        print(self.raw_data)
---
+++ Main.py
@@ -0,0 +1,4 @@
+from ExcelReader import ExcelReader
+
+reader = ExcelReader("test data.xlsx")
+print(reader.read_data())
